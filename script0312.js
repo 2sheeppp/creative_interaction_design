44a362c3afc545dc6e9dae1dfd9e8268757290ce
2sheeppp <161456251+2sheeppp@users.noreply.github.com>
2024-03-12 21:23:15 +0800

    0312第5次课堂练习

---
+++ script0312.js
@@ -0,0 +1,11 @@
+document.write("Hello world!");
+function SayHello(){
+  var response = prompt("What is your name?");
+  alert("Hello " + response + ",have a nice day!");
+};
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+};
